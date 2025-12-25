97cda2817696917439b6636ab848621c72ebd87f
leahZinger <workleah15@gmail.com>
2025-12-25 15:45:49 +0200

    update

--- ToDoListReact/src/service.js
+++ ToDoListReact/src/service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-  baseURL: "https://todo-server-bwjl.onrender.com"
+  baseURL: process.env.REACT_APP_API_URL
 });
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
